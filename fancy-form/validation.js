7cb1bb7bba834322932c2a7d7019e7d38f090af8
Yunf.li37@gmail.com <Yunf.li37@gmail.com>
2017-10-04 21:06:42 -0400

    basic validation

--- fancy-form/validation.js
+++ fancy-form/validation.js
@@ -1,8 +1,15 @@
 $(document).ready(()=>{
-
-	$("#sign-up-form").submit((event)=>{
+	$(".sign-up-form").submit((event)=>{
 		event.preventDefault();
-		var inputValue = $("input").val();
-		console.log(inputValue);
+		$("input").each(function(event){
+			if($(this).val() == ""){
+				$(this).addClass("fill-out");
+			}
+		});
+		$("textarea").each(function(event){
+			if($(this).val().length <5){
+				console.log("you need more than 5 characters");
+			}
+		});
 	});
 });
